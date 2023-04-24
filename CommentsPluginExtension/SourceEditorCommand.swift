0e554de4cb70390399267b277b1b82129dd9ae15
chunqian.shen <chunqian.shen@qq.com>
2023-04-24 17:55:30 +0800

    Fix

--- CommentsPluginExtension/SourceEditorCommand.swift
+++ CommentsPluginExtension/SourceEditorCommand.swift
@@ -12,16 +12,16 @@ import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
-    private var cursorShift: Int = 0
-    
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         
-        cursorShift = 0
         print("--------------command start--------------")
         let buffer = invocation.buffer
         let selections = buffer.selections
-        let numberOfSelections = selections.count
         let lines = buffer.lines
+        
+        var startSelection: (start: Int, end: Int) = (0, 0)
+        var endSelection: (start: Int, end: Int) = (0, 0)
+        var numberOfSelectionLines: Int = 0
         
         guard let startRange = selections.firstObject as? XCSourceTextRange,
             let endRange = selections.lastObject as? XCSourceTextRange
@@ -29,6 +29,10 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             completionHandler(nil)
             return
         }
+        
+        startSelection = (startRange.start.column, startRange.end.column)
+        endSelection = (endRange.start.column, endRange.end.column)
+        
         
         print("start at \(startRange)")
         print("end at \(endRange)")
@@ -41,12 +45,13 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         } else {
             endLine = endRange.end.line
         }
+        numberOfSelectionLines = endLine - startLine
         
         var shouldComment = false
         var commentIndex: Int? = nil
         
         // 单行
-        if numberOfSelections == 1 {
+        if numberOfSelectionLines == 0 {
             for index in startLine...endLine {
                 guard let line = lines.object(at: index) as? NSString else { continue }
                 let code = line.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -70,21 +75,27 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                     
                     if shouldComment {
                         lines[index] = linePrefix + "// " + code
-                        cursorShift += 3
+                        if startSelection.start > linePrefix.count {
+                            startSelection.start = min(startSelection.start + 3, (lines[index] as? String)?.count ?? 0)
+                            startSelection.end = min(startSelection.end + 3, (lines[index] as? String)?.count ?? 0)
+                        }
                     } else if !shouldComment && commented {
                         let uncommentedCode = code.replacingOccurrences(of: "^// ?", with: "", options: .regularExpression)
                         lines[index] = linePrefix + uncommentedCode
-                        cursorShift -= 3
+                        if startSelection.start > linePrefix.count {
+                            startSelection.start = max(startSelection.start - 3, commentIndex!)
+                            startSelection.end = max(startSelection.end - 3, commentIndex!)
+                        }
                     }
                 }
             }
             
-            startRange.start.column += cursorShift
-            endRange.end.column += cursorShift
+            startRange.start.column = startSelection.start
+            startRange.end.column = startSelection.end
         }
         
         // 多行
-        if numberOfSelections > 1 {
+        if numberOfSelectionLines > 0 {
             for index in startLine...endLine {
                 guard let line = lines.object(at: index) as? NSString else { continue }
                 let code = line.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -112,17 +123,41 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                     
                     if shouldComment {
                         lines[index] = linePrefix + "// " + code
-                        cursorShift += 3
+                        if index == startLine {
+                            if startSelection.start > linePrefix.count {
+                                startSelection.start = min(startSelection.start + 3, (lines[index] as? String)?.count ?? 0)
+                                startSelection.end = min(startSelection.end + 3, (lines[index] as? String)?.count ?? 0)
+                            }
+                        }
+                        if index == endLine {
+                            if endSelection.start > linePrefix.count {
+                                endSelection.start = min(endSelection.start + 3, (lines[index] as? String)?.count ?? 0)
+                                endSelection.end = min(endSelection.end + 3, (lines[index] as? String)?.count ?? 0)
+                            }
+                        }
                     } else if !shouldComment && commented {
                         let uncommentedCode = code.replacingOccurrences(of: "^// ?", with: "", options: .regularExpression)
                         lines[index] = linePrefix + uncommentedCode
-                        cursorShift -= 3
+                        if index == startLine {
+                            if startSelection.start > linePrefix.count {
+                                startSelection.start = max(startSelection.start - 3, commentIndex!)
+                                startSelection.end = max(startSelection.end - 3, commentIndex!)
+                            }
+                        }
+                        if index == endLine {
+                            if endSelection.start > linePrefix.count {
+                                endSelection.start = max(endSelection.start - 3, commentIndex!)
+                                endSelection.end = max(endSelection.end - 3, commentIndex!)
+                            }
+                        }
                     }
                 }
             }
             
-            startRange.start.column += cursorShift
-            endRange.end.column += cursorShift
+            startRange.start.column = startSelection.start
+            startRange.end.column = startSelection.end
+            endRange.start.column = endSelection.start
+            endRange.end.column = endSelection.end
         }
         
         completionHandler(nil)
